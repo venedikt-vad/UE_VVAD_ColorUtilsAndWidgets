3e1d3a90c61f9f2d4621cdb0298c8eb1753d4cdc
VVAD <venya_kozlov_00@mail.ru>
2025-12-28 20:40:41 +0600

    Plugin created

---
+++ Source/VVAD_ColorUtilsAndWidgets/Private/VVAD_ColorUtilsAndWidgets.cpp
@@ -0,0 +1,22 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VVAD_ColorUtilsAndWidgets.h"
+
+#define LOCTEXT_NAMESPACE "FVVAD_ColorUtilsAndWidgetsModule"
+
+void FVVAD_ColorUtilsAndWidgetsModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	
+}
+
+void FVVAD_ColorUtilsAndWidgetsModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+	
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FVVAD_ColorUtilsAndWidgetsModule, VVAD_ColorUtilsAndWidgets)
---
+++ Source/VVAD_ColorUtilsAndWidgets/Private/VVAD_ColorUtilsAndWidgetsBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VVAD_ColorUtilsAndWidgetsBPLibrary.h"
+#include "VVAD_ColorUtilsAndWidgets.h"
+
+UVVAD_ColorUtilsAndWidgetsBPLibrary::UVVAD_ColorUtilsAndWidgetsBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UVVAD_ColorUtilsAndWidgetsBPLibrary::VVAD_ColorUtilsAndWidgetsSampleFunction(float Param)
+{
+	return -1;
+}
+
